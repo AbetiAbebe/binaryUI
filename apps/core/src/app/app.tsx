920551f6deb12031f8a1c7e2438cfece13f76883
Abeti_abebe <abenezer.abeti@gmail.com>
2022-12-12 20:20:08 +0300

    ts update

--- apps/core/src/app/app.tsx
+++ apps/core/src/app/app.tsx
@@ -1,11 +1,18 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
+import { CoreAuth } from '@binarycosmo/core/auth';
+import { CoreHome } from '@binarycosmo/core/home';
+import { BrowserRouter as Router, Route, Link, BrowserRouter, Routes } from 'react-router-dom';
 
 export function App() {
   return (
     <>
-      <NxWelcome title="core" />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<CoreHome />}></Route>
+        <Route path="/login" element={<CoreAuth />}></Route>
+      </Routes>
+    </BrowserRouter>
+      {/* <CoreHome /> */}
       <div />
     </>
   );
