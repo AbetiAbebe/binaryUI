44f8b135e2964545c55dfac7756b79ff07ec1b08
joseph <121518979+eyosaftba@users.noreply.github.com>
2023-01-02 11:48:49 +0300

    latest

--- apps/core/src/app/app.tsx
+++ apps/core/src/app/app.tsx
@@ -1,30 +1,45 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { CoreHome } from '@binarycosmo/core/home';
-import {LoginForm , RegistrationForm, RegistrationResult, CoreAuth} from '@binarycosmo/core/auth';
-import { BrowserRouter as Router, Route, Link, BrowserRouter, Routes } from 'react-router-dom';
+import {
+  LoginForm,
+  RegistrationForm,
+  RegistrationResult,
+  CoreAuth,
+} from '@binarycosmo/core/auth';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  BrowserRouter,
+  Routes,
+} from 'react-router-dom';
 import { CoreLayout, Dashboard } from '@binarycosmo/core/layout';
 import { CheckOutFront, Shared } from '@binarycosmo/shared';
-
+import { CoreCourses } from '@binarycosmo/core/courses';
 
 export function App() {
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/home" element={<CoreHome />}></Route>
-      <Route path="/" element={<CoreAuth />}>
-        <Route path="/login" element={<LoginForm />}></Route>
-        <Route path="/register" element={<RegistrationForm />}></Route>
-        <Route path="/result" element={<RegistrationResult />}></Route>
-      </Route>
-      <Route path="/checkout" element={ <Shared /> }></Route>
-      <Route path="/" element={<CoreHome />} />
-      <Route path="/dashboard" element={<CoreLayout />}>
-        <Route path="/dashboard/home" element={<Dashboard />}></Route>
-        <Route path="/dashboard/studList" element={<RegistrationForm />}></Route>
-      </Route>
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<CoreHome />}></Route>
+          <Route path="/" element={<CoreAuth />}>
+            <Route path="/login" element={<LoginForm />}></Route>
+            <Route path="/register" element={<RegistrationForm />}></Route>
+            <Route path="/result" element={<RegistrationResult />}></Route>
+          </Route>
+          <Route path="/checkout" element={<Shared />}></Route>
+          <Route path="/course" element={<CoreCourses />} />
+          <Route path="/" element={<CoreHome />} />
+          <Route path="/dashboard" element={<CoreLayout />}>
+            <Route path="/dashboard/home" element={<Dashboard />}></Route>
+            <Route
+              path="/dashboard/studList"
+              element={<RegistrationForm />}
+            ></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
       <div />
     </>
   );
